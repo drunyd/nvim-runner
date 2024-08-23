1a0e5034547eb5ac765a30b5e990a4416601820e
Balazs Papp <bapapp@gmail.com>
2024-08-23 22:49:59 +0200

    init tidied

--- lua/runner/init.lua
+++ lua/runner/init.lua
@@ -1,21 +1,14 @@
--- lua/runner/init.lua
 
-local M = {}
-
-function M.setup()
   -- Print a message to verify that setup is running
   print("Running setup() function for runner plugin")
   
   -- Define key mapping for <leader>str
   vim.api.nvim_set_keymap('n', '<leader>str', '<cmd>lua require("runner.runner").run()<CR>', { noremap = true, silent = true })
-end
 
 -- Automatically call setup when the module is required
-M.setup()
 
 -- Create a Neovim command to call the run function
 vim.api.nvim_create_user_command('RunnerRun', function()
   require('runner.runner').run()
 end, { desc = 'Run the Runner plugin function' })
 
-return M
