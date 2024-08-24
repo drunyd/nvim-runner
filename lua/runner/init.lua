4b30c7f8ea0fbd1ec644fd7793c218fd156d1d8a
Balazs Papp <bapapp@gmail.com>
2024-08-24 10:55:40 +0200

    removed lua2

--- lua/runner/init.lua
+++ lua/runner/init.lua
@@ -129,13 +129,13 @@ function M.setup()
   print("Running setup() function for runner plugin")
   
   -- Define key mapping for <leader>str
-  vim.api.nvim_set_keymap('n', '<leader>str', '<cmd>lua require("runner.runner").run()<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<leader>str', '<cmd>lua require("runner.init").run()<CR>', { noremap = true, silent = true })
 
 -- Automatically call setup when the module is required
 
 -- Create a Neovim command to call the run function
 vim.api.nvim_create_user_command('RunnerRun', function()
-  require('runner.runner').run()
+  require('runner.init').run()
 end, { desc = 'Run the Runner plugin function' })
 end
 
